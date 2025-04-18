287f8ecae1a63f361c9a387b677a84c7914352cc
root <root@DESKTOP-Q76UBJC>
2025-04-18 21:57:18 +0500

    added api for video_gen

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
 import replicate
 from pydantic import BaseModel
 from fastapi import FastAPI,UploadFile,Form
+from fastapi.responses import JSONResponse
 
 class ttsForm(BaseModel):
 	audio: UploadFile
 	text: str
+
+class videogenForm(BaseModel):
+	img: UploadFile
+	audio: UploadFile
 
 app=FastAPI()
 
@@ -28,4 +33,44 @@ async def tts(formData:ttsForm = Form()):
 	)
 	with open("output.wav", "wb") as file:
     		file.write(output.read())
+
+
+	return JSONResponse(content={"message": "Audio generated at output.wav"})
+
+@app.post("/video_gen")
+async def video_gen(formData:videogenForm = Form()):
+	with open("src_audio.wav",'wb') as file:
+                audioData = await formData.audio.read()
+                file.write(audioData)
+
+	with open("src_img.jpg",'wb') as file:
+                imgData = await formData.img.read()
+                file.write(imgData)
+
+	driven_audio = open("src_audio.wav",'rb');
+	src_img = open("src_img.jpg",'rb');
+
+	input={
+        	"facerender": "facevid2vid",
+        	"pose_style": 0,
+        	"preprocess": "crop",
+        	"still_mode": True,
+        	"driven_audio": driven_audio,
+        	"source_image": src_img,
+        	"use_enhancer": True,
+        	"size_of_image": 256,
+        	"expression_scale": 1
+    		}
+
+	output = replicate.run(
+    		"cjwbw/sadtalker:a519cc0cfebaaeade068b23899165a11ec76aaa1d2b313d40d214f204ec957a3",
+		input=input
+	)
+
+
+	with open("output.mp4", "wb") as file:
+                file.write(output.read())
+
+
+	return JSONResponse(content={"message": "Video generated at output.mp4"})
 
